7cc825d367f3a0c595012ddbce168614e4f2f168
Victor Ayala <victorayala1996@gmail.com>
2021-04-19 20:19:15 -0400

    Update modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useRef, useEffect, useCallback} from 'react';
+import {useSpring, animated} from 'react-spring';
 import styled from 'styled-components';
+import { MdClose } from 'react-icons/md';
 
 const Background = styled.div`
 width: 100%;
@@ -60,8 +62,62 @@ width: 32px;
 height: 32px;
 padding: 0
 z-index: 10;
-`
-
-export const Modal = ({showModal, setShowModal}) =>{
-    return <>{showModal ? <div>Modal</div> : null}</>;
-}
+`;
+export const Modal = ({ showModal, setShowModal }) => {
+    const modalRef = useRef();
+  
+    const animation = useSpring({
+      config: {
+        duration: 250
+      },
+      opacity: showModal ? 1 : 0,
+      transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+    });
+  
+    const closeModal = e => {
+      if (modalRef.current === e.target) {
+        setShowModal(false);
+      }
+    };
+  
+    const keyPress = useCallback(
+      e => {
+        if (e.key === 'Escape' && showModal) {
+          setShowModal(false);
+          console.log('I pressed');
+        }
+      },
+      [setShowModal, showModal]
+    );
+  
+    useEffect(
+      () => {
+        document.addEventListener('keydown', keyPress);
+        return () => document.removeEventListener('keydown', keyPress);
+      },
+      [keyPress]
+    );
+  
+    return (
+      <>
+        {showModal ? (
+          <Background onClick={closeModal} ref={modalRef}>
+            <animated.div style={animation}>
+              <ModalWrapper showModal={showModal}>
+              <ModalImg src={require('./modal.jpg')} alt='camera' />                
+              <ModalContent>
+                  <h1>Are you ready?</h1>
+                  <p>Get exclusive access to our next launch.</p>
+                  <button>Join Now</button>
+                </ModalContent>
+                <CloseModalButton
+                  aria-label='Close modal'
+                  onClick={() => setShowModal(prev => !prev)}
+                />
+              </ModalWrapper>
+            </animated.div>
+          </Background>
+        ) : null}
+      </>
+    );
+  };
